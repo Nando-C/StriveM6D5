9ae42ef6e58e9121b639c7b9eb322590be83d059
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-07-09 11:11:04 +0100

    adds basic comments/reviews routes CRUD

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -83,4 +83,110 @@ productsRouter.delete('/:productId', async (req, res, next) => {
     }
 })
 
+// *****************************************************************************
+//                                 COMMENTS
+// *****************************************************************************
+
+// =========  CREATES A NEW COMMENT ON A PRODUCT =============
+
+productsRouter.post('/:productId', async (req, res, next) => {
+    try {
+        const commentToInsert = {...req.body, createdAt: new Date(), updatedAt: new Date()}
+
+        const productId = req.params.productId
+        const updatedProduct = await ProductModel.findByIdAndUpdate(productId, { $push: {comments: commentToInsert}}, { new: true, runValidators: true })
+
+        if(updatedProduct) {
+            res.send(updatedProduct)
+        } else {
+            next(createError(404, `Product with _id ${productId} Not Found!`))
+        }
+    } catch (error) {
+        if(error.name === "validationError") {
+            next(createError(400, error))
+        } else {
+        next(createError(500, `An Error ocurred while creating a comment on product ID: ${req.params.productId}`))
+        }
+    }
+})
+
+// =========  RETRIEVES A LIST OF COMMENTS FROM A PRODUCT =============
+
+productsRouter.get('/:productId/comments', async (req, res, next) => {
+    try {
+        const productId = req.params.productId
+        const product = await ProductModel.findById(productId)
+
+        if (product) {
+            res.send(product.comments)
+        } else {
+            next(createError(404, `Product with _id ${productId} Not Found!`))
+        }
+    } catch (error) {
+        next(createError(500, "An Error ocurred while getting the list of comments"))
+    }
+})
+
+// =========  RETRIEVES A SINGLE COMMENT FROM A PRODUCT =============
+
+productsRouter.get('/:productId/comments/:commentId', async (req, res, next) => {
+    try {
+        const productId = req.params.productId
+        const commentId = req.params.commentId
+        const productComment = await ProductModel.findById(productId, { comments: { $elemMatch: { _id: commentId}}})
+        
+        if (productComment) {
+            if (productComment.comments.length > 0) {
+                res.send(productComment.comments[0])
+            } else {
+                next(createError(404, `Comment with _id ${commentId} Not Found!`))
+            }
+
+        } else {
+            next(createError(404, `Product with _id ${productId} Not Found!`))
+        }
+    } catch (error) {
+        next(createError(500, `An Error ocurred while getting comment with ID: ${req.params.commentId}`))
+    }
+})
+
+// =========  DELETES A COMMENT FROM A PRODUCT =============
+
+productsRouter.delete('/:productId/comments/:commentId', async (req, res, next) => {
+    try {
+        const productId = req.params.productId
+        const commentId = req.params.commentId
+        const product = await ProductModel.findByIdAndUpdate(productId, { $pull: { comments: {_id: commentId}}}, { new: true })
+
+        if (product) {
+            res.send(product)
+        } else {
+            next(createError(404, `Product with _id ${productId} Not Found!`))
+        }
+    } catch (error) {
+        next(createError(500, `An Error ocurred while deleting comment with ID: ${req.params.commentId}`))
+    }
+})
+
+// =========  UPDATES A COMMENT ON A PRODUCT =============
+
+productsRouter.put('/:productId/comments/:commentId', async (req, res, next) => {
+    try {
+        const productId = req.params.productId
+        const commentId = req.params.commentId
+
+        const product = await ProductModel.findOneAndUpdate( 
+            { _id: productId , "comments._id": commentId, }, 
+            { $set: { "comments.$": req.body, }}, 
+            { new: true, runValidators: true, }
+        )
+        if (product) {
+            res.send(product)
+        } else {
+            next(createError(404, `Product with _id ${productId} Not Found!`))
+        }
+    } catch (error) {
+        next(createError(500, `An Error ocurred while updating comment with ID: ${req.params.commentId}`))
+    }
+})
 export default productsRouter
